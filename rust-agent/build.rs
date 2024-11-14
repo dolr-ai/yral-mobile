aba84a7b5e264967a3f7a3dca817075946b81b07
sarvesh-sharma-yral <sarvesh@gobazzinga.io>
2024-11-14 19:56:46 +0530

    build: Adds ffi for sns_governance (#26)

--- rust-agent/build.rs
+++ rust-agent/build.rs
@@ -7,7 +7,8 @@ fn main() {
         "src/lib.rs",
         "src/individual_user_template/individual_user_template_ffi.rs",
         "src/platform_orchestrator/platform_orchestrator_ffi.rs",
-        "src/post_cache/post_cache_ffi.rs"
+        "src/post_cache/post_cache_ffi.rs",
+        "src/sns_governance/sns_governance_ffi.rs"
     ];
     for path in &bridges {
         println!("cargo:rerun-if-changed={}", path);
