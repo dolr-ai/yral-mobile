9e3d765cffda9ac26ddc32ac864826eb933b7e78
sarveshsharma <sarvesh@gobazzinga.io>
2024-11-26 22:30:42 +0530

    build: Add pre commit hooks for ktlint and detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     //trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidApplication).apply(false)
@@ -11,4 +13,29 @@ plugins {
 subprojects {
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
     apply(plugin = "io.gitlab.arturbosch.detekt" )
+    tasks.withType<Detekt>().configureEach {
+        jvmTarget = "17" // Set to a supported JVM target version
+    }
+}
+
+tasks.register("copyGitHooks", Copy::class.java) {
+    description = "Copies the git hooks from /git-hooks to the .git folder."
+    group = "git hooks"
+    from("$rootDir/scripts/pre-commit")
+    into("$rootDir/.git/hooks/")
+}
+tasks.register("installGitHooks", Exec::class.java) {
+    description = "Installs the pre-commit git hooks from /git-hooks."
+    group = "git hooks"
+    workingDir = rootDir
+    commandLine = listOf("chmod")
+    args("-R", "+x", ".git/hooks/")
+    dependsOn("copyGitHooks")
+    doLast {
+        logger.info("Git hook installed successfully.")
+    }
+}
+
+afterEvaluate {
+    tasks.getByPath(":shared:preBuild").dependsOn(":installGitHooks")
 }
