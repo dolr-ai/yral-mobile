3cbef0ac30e0ff739a58da5d40bf1ab681416ef3
sarvesh-sharma-yral <sarvesh@gobazzinga.io>
2025-01-05 17:34:18 +0530

    Adds profile UI (#92)
    * fix: Fixes Yral player caching issues
    
    * build: Adds Profile page

---
+++ iosApp/iosApp/DesignSystem/SwiftUI/URLImage.swift
@@ -0,0 +1,38 @@
+//
+//  URLImage.swift
+//  iosApp
+//
+//  Created by Sarvesh Sharma on 04/01/25.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+import SwiftUI
+
+struct URLImage: View {
+  let url: URL?
+  @State private var imageData: Data?
+
+  var body: some View {
+    Group {
+      if let imageData, let uiImage = UIImage(data: imageData) {
+        Image(uiImage: uiImage)
+          .resizable()
+          .scaledToFit()
+      } else {
+        ProgressView()
+      }
+    }
+    .onAppear {
+      loadImage()
+    }
+  }
+
+  private func loadImage() {
+    guard let url else { return }
+    URLSession.shared.dataTask(with: url) { data, _, error in
+      guard let data, error == nil else { return }
+      DispatchQueue.main.async {
+        self.imageData = data
+      }
+    }.resume()
+  }
+}
