99f54696e79583157fc6321f2dccf3ed811a706b
sarvesh-sharma-yral <sarvesh@gobazzinga.io>
2025-01-07 21:21:52 +0530

    Fixes initial load time and adds like/dislike capability (#98)
    * build: Removes intial load time and adds like/dislike capability
    
    * fix: Removes duplicate elements from feed

---
+++ iosApp/iosApp/Feed/Domain/Entities/LikeQuery.swift
@@ -0,0 +1,13 @@
+//
+//  LikeQuery.swift
+//  iosApp
+//
+//  Created by Sarvesh Sharma on 07/01/25.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+struct LikeQuery {
+  let postID: Int
+  let canisterID: String
+  let index: Int
+}
