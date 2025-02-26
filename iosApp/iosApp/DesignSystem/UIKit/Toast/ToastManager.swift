7949be076df70ac10ba670bef5ba8ad49e67c18e
sarveshsharma <sarvesh@gobazzinga.io>
2025-02-26 23:17:17 +0530

    build: Adds toast view

---
+++ iosApp/iosApp/DesignSystem/UIKit/Toast/ToastManager.swift
@@ -0,0 +1,54 @@
+//
+//  ToastManager.swift
+//  iosApp
+//
+//  Created by Sarvesh Sharma on 26/02/25.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import UIKit
+
+final class ToastManager {
+  static func showToast(type: ToastType, onRetry: (() -> Void)? = nil) {
+    guard let windowScene = UIApplication.shared.connectedScenes
+      .compactMap({ $0 as? UIWindowScene })
+      .first(where: { $0.activationState == .foregroundActive }),
+          let window = windowScene.windows.first(where: { $0.isKeyWindow }) else {
+      return
+    }
+
+    let safeAreaInsets = window.safeAreaInsets
+    let toastWidth = window.bounds.width - (Constants.toastWidthDelta + safeAreaInsets.left + safeAreaInsets.right)
+    let toastX = safeAreaInsets.left + (Constants.toastWidthDelta / 2)
+    let toastView = ToastView(type: type, buttonAction: onRetry)
+
+    toastView.frame = CGRect(
+      x: toastX,
+      y: Constants.toastInitialY,
+      width: toastWidth,
+      height: .zero
+    )
+    window.addSubview(toastView)
+    toastView.layoutIfNeeded()
+
+    let targetSize = CGSize(width: toastWidth, height: UIView.layoutFittingCompressedSize.height)
+    let finalHeight = toastView.systemLayoutSizeFitting(targetSize).height
+    toastView.frame = CGRect(
+      x: toastX,
+      y: -finalHeight,
+      width: toastWidth,
+      height: finalHeight
+    )
+
+    UIView.animate(withDuration: CGFloat.animationPeriod) {
+      toastView.frame.origin.y = safeAreaInsets.top
+    }
+  }
+}
+
+extension ToastManager {
+  enum Constants {
+    static let toastInitialY: CGFloat = -1000
+    static let toastWidthDelta: CGFloat = 32
+  }
+}
---
+++ iosApp/iosApp/DesignSystem/UIKit/Toast/ToastView.swift
@@ -0,0 +1,150 @@
+//
+//  ToastView.swift
+//  iosApp
+//
+//  Created by Sarvesh Sharma on 26/02/25.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+import UIKit
+
+final class ToastView: UIView {
+
+  private let type: ToastType
+  private let buttonAction: (() -> Void)?
+
+  private let iconImageView = getUIImageView()
+  private let titleLabel = getUILabel()
+  private let subtitleLabel = getUILabel()
+  private let actionButton = getUIButton()
+  private let closeButton = UIButton(type: .system)
+
+  init(type: ToastType, buttonAction: (() -> Void)? = nil) {
+    self.type = type
+    self.buttonAction = buttonAction
+    super.init(frame: .zero)
+
+    backgroundColor = type.backgroundColor
+    layer.masksToBounds = true
+    layer.cornerRadius = Constants.cornerRadius
+    setupCloseButton()
+    setupIconImageView()
+    setupTitleLabel()
+    setupSubtitleLabel()
+    setupActionButtonIfNeeded()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func setupCloseButton() {
+    closeButton.setImage(Constants.closeButtonImage, for: .normal)
+    closeButton.tintColor = .white
+    closeButton.addTarget(self, action: #selector(handleCloseButtonTapped), for: .touchUpInside)
+    addSubview(closeButton)
+
+    closeButton.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      closeButton.topAnchor.constraint(equalTo: topAnchor, constant: Constants.topPadding),
+      closeButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Constants.horizontalPadding)
+    ])
+  }
+
+  private func setupIconImageView() {
+    iconImageView.contentMode = .scaleAspectFit
+    iconImageView.image = type.icon
+    addSubview(iconImageView)
+
+    NSLayoutConstraint.activate([
+      iconImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.horizontalPadding),
+      iconImageView.topAnchor.constraint(equalTo: topAnchor, constant: Constants.topPadding),
+      iconImageView.widthAnchor.constraint(equalToConstant: Constants.iconSize),
+      iconImageView.heightAnchor.constraint(equalToConstant: Constants.iconSize)
+    ])
+  }
+
+  private func setupTitleLabel() {
+    titleLabel.text = type.title
+    titleLabel.font = Constants.titleLabelFont
+    titleLabel.textColor = Constants.titleLabelTextColor
+    addSubview(titleLabel)
+
+    NSLayoutConstraint.activate([
+      titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: Constants.topPadding),
+      titleLabel.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor, constant: Constants.spacing),
+      titleLabel.trailingAnchor.constraint(
+        lessThanOrEqualTo: closeButton.leadingAnchor,
+        constant: -Constants.spacing
+      )
+    ])
+  }
+
+  private func setupSubtitleLabel() {
+    subtitleLabel.text = type.subtitle
+    subtitleLabel.font = Constants.subtitleLabelFont
+    subtitleLabel.textColor = Constants.subtitleLabelTextColor
+    subtitleLabel.numberOfLines = .zero
+    addSubview(subtitleLabel)
+
+    NSLayoutConstraint.activate([
+      subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: .two),
+      subtitleLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+      subtitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Constants.horizontalPadding)
+    ])
+  }
+
+  private func setupActionButtonIfNeeded() {
+    guard let btnTitle = type.buttonTitle else {
+      NSLayoutConstraint.activate([
+        subtitleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -Constants.topPadding)
+      ])
+      return
+    }
+
+    actionButton.setTitle(btnTitle, for: .normal)
+    actionButton.setTitleColor(Constants.retryButtonTextColor, for: .normal)
+    actionButton.titleLabel?.font = Constants.retryButtonFont
+    actionButton.addTarget(self, action: #selector(handleActionButtonTapped), for: .touchUpInside)
+    addSubview(actionButton)
+
+    NSLayoutConstraint.activate([
+      actionButton.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: Constants.spacing),
+      actionButton.leadingAnchor.constraint(equalTo: subtitleLabel.leadingAnchor),
+      actionButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -Constants.topPadding)
+    ])
+  }
+
+  @objc private func handleActionButtonTapped() {
+    buttonAction?()
+    dismiss()
+  }
+
+  @objc private func handleCloseButtonTapped() {
+    dismiss()
+  }
+
+  private func dismiss() {
+    UIView.animate(withDuration: 0.3, animations: {
+      self.frame.origin.y = -self.frame.height
+    }, completion: { _ in
+      self.removeFromSuperview()
+    })
+  }
+}
+
+extension ToastView {
+  enum Constants {
+    static let closeButtonImage = UIImage(named: "toast_close")
+    static let titleLabelTextColor = UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1)
+    static let titleLabelFont = UIFont(name: "KumbhSans-Medium", size: 14)
+    static let subtitleLabelTextColor = UIColor(red: 0.831, green: 0.831, blue: 0.831, alpha: 1)
+    static let subtitleLabelFont = UIFont(name: "KumbhSans-Regular", size: 14)
+    static let retryButtonTextColor = UIColor(red: 0.671, green: 0.188, blue: 0.137, alpha: 1)
+    static let retryButtonFont = UIFont(name: "KumbhSans-Medium", size: 14)
+    static let cornerRadius = 6.0
+    static let topPadding: CGFloat = 12
+    static let horizontalPadding: CGFloat = 16
+    static let spacing: CGFloat = 8
+    static let iconSize: CGFloat = 24
+  }
+}
