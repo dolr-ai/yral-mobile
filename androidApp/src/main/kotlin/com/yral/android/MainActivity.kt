ded9725e13ec23b8c6d578216bc80a05fca3335d
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2025-04-15 20:06:23 +0530

    Prepares rust library for iOS module (#159)
    * chore: Prepares rust library for iOS module
    
    * fix: Fixes workflow for preview apk

--- androidApp/src/main/kotlin/com/yral/android/MainActivity.kt
+++ androidApp/src/main/kotlin/com/yral/android/MainActivity.kt
@@ -5,12 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.yral.shared.core.AndroidPlatformResources
 import com.yral.shared.core.PlatformResourcesHolder
-import com.yral.shared.uniffi.generated.nativeActivityCreate
+import com.yral.shared.uniffi.generated.initRustLogger
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        nativeActivityCreate()
+        initRustLogger()
         PlatformResourcesHolder.initialize(AndroidPlatformResources(this))
         setContent {
             Root()
