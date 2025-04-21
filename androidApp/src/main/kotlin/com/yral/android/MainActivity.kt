4b129ec2c6a0fb89666842a933668943cd9c07a4
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-04-21 13:21:54 +0530

    build: Handles PlatformResources via DI

--- androidApp/src/main/kotlin/com/yral/android/MainActivity.kt
+++ androidApp/src/main/kotlin/com/yral/android/MainActivity.kt
@@ -5,17 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.yral.android.analytics.initAnalyticsManager
 import com.yral.shared.core.AndroidPlatformResources
-import com.yral.shared.core.PlatformResourcesHolder
+import com.yral.shared.core.PlatformResourcesFactory
+import com.yral.shared.koin.koinInstance
 import com.yral.shared.uniffi.generated.initRustLogger
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        PlatformResourcesHolder.initialize(AndroidPlatformResources(this))
+        initPlatformResources()
         initRustLogger()
         initAnalyticsManager()
         setContent {
             Root()
         }
+    }
+
+    private fun initPlatformResources() {
+        val platformResourcesFactory: PlatformResourcesFactory = koinInstance.get()
+        platformResourcesFactory.initialize(AndroidPlatformResources(this))
     }
 }
