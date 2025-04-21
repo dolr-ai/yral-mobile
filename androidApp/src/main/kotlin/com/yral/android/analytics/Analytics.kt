033ee39da391cccdb67f6843ff81cd335bbf67c2
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-04-21 13:21:54 +0530

    build: Handles PlatformResources via DI

--- androidApp/src/main/kotlin/com/yral/android/analytics/Analytics.kt
+++ androidApp/src/main/kotlin/com/yral/android/analytics/Analytics.kt
@@ -3,14 +3,15 @@ package com.yral.android.analytics
 import com.yral.shared.analytics.core.AnalyticsManager
 import com.yral.shared.analytics.core.ApiClient
 import com.yral.shared.analytics.core.CoreService
-import com.yral.shared.core.PlatformResourcesHolder.platformResources
+import com.yral.shared.core.PlatformResourcesFactory
 import com.yral.shared.koin.koinInstance
 
 fun initAnalyticsManager() {
     val analyticsManager: AnalyticsManager = koinInstance.get()
+    val platformResourcesFactory: PlatformResourcesFactory = koinInstance.get()
     analyticsManager.addProvider(
         FirebaseAnalyticsProvider(
-            context = platformResources.applicationContext,
+            context = platformResourcesFactory.resources().applicationContext,
             eventFilter = { event ->
                 // Only track specific events to Firebase
                 event.name.startsWith("firebase_")
--- androidApp/src/main/kotlin/com/yral/android/di/CoreModule.kt
+++ androidApp/src/main/kotlin/com/yral/android/di/CoreModule.kt
@@ -1,7 +1,8 @@
 package com.yral.android.di
 
 import com.yral.shared.core.AppDispatchers
-import com.yral.shared.core.PlatformResourcesHolder.platformResources
+import com.yral.shared.core.PlatformResourcesFactory
+import com.yral.shared.koin.koinInstance
 import com.yral.shared.preferences.provideSharedPreferences
 import org.koin.dsl.module
 
@@ -9,11 +10,12 @@ private const val USER_SHARED_PREF_NAME = "YRAL_PREF"
 
 internal val coreModule =
     module {
+        single { PlatformResourcesFactory() }
         single { AppDispatchers() }
         single {
             provideSharedPreferences(
                 preferenceName = USER_SHARED_PREF_NAME,
-                platformResources = platformResources,
+                platformResourcesFactory = koinInstance.get(),
             )
         }
     }
