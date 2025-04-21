9cdca0b1aa6287170b2faa88fcede344fb64730b
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-04-21 12:17:24 +0530

    build: Introduces Dispatchers

--- shared/core/build.gradle.kts
+++ shared/core/build.gradle.kts
@@ -24,6 +24,7 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
+            implementation(libs.kotlinx.coroutines.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
