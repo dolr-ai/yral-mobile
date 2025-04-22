424cd3bb79ccfb5114c77527c9dd30e96bc56e24
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-04-22 11:52:19 +0530

    chore: Improves code to create Http client

--- androidApp/src/main/kotlin/com/yral/android/di/NetworkModule.kt
+++ androidApp/src/main/kotlin/com/yral/android/di/NetworkModule.kt
@@ -1,10 +1,10 @@
 package com.yral.android.di
 
-import com.yral.shared.http.HttpClientFactory
+import com.yral.shared.http.createClient
 import com.yral.shared.koin.koinInstance
 import org.koin.dsl.module
 
 internal val networkModule =
     module {
-        single { HttpClientFactory.getInstance(koinInstance.get()).build() }
+        single { createClient(koinInstance.get()) }
     }
