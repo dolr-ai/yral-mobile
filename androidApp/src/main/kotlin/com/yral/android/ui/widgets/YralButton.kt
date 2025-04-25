7b51e13f0e6a1875e337be60e9a18ba98d85cee1
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-04-25 12:25:14 +0530

    fix: Removes padding from YralButton

--- androidApp/src/main/kotlin/com/yral/android/ui/widgets/YralButton.kt
+++ androidApp/src/main/kotlin/com/yral/android/ui/widgets/YralButton.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -31,12 +30,7 @@ fun YralButton(
     Row(
         modifier =
             Modifier
-                .padding(
-                    start = 20.dp,
-                    top = 12.dp,
-                    end = 20.dp,
-                    bottom = 12.dp,
-                ).fillMaxWidth()
+                .fillMaxWidth()
                 .height(45.dp)
                 .drawWithCache {
                     val gradient =
