8aaee15ea38001914a7eb03032a60cefd914522e
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-04-25 11:11:07 +0530

    build: Adds gradient to YralButton

--- androidApp/src/main/kotlin/com/yral/android/ui/design/YralColors.kt
+++ androidApp/src/main/kotlin/com/yral/android/ui/design/YralColors.kt
@@ -1,8 +1,15 @@
+@file:Suppress("MagicNumber")
+
 package com.yral.android.ui.design
 
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import kotlin.math.cos
+import kotlin.math.hypot
+import kotlin.math.sin
 
-@Suppress("MagicNumber")
 object YralColors {
     val PrimaryContainer: Color = Color(0xFF0A0A0A)
     val OnPrimaryContainer: Color = Color(0xFFFAFAFA)
@@ -17,4 +24,27 @@ object YralColors {
     val Pink300: Color = Color(0xFFE2017B)
 
     val Divider: Color = Color(0xFF232323)
+}
+
+fun pinkGradient(size: Size): Brush {
+    val angle = Math.toRadians(128.273)
+    val radius = hypot(size.width, size.height) / 2f
+    val center = Offset(size.width / 2f, size.height / 2f)
+
+    val dx = cos(angle).toFloat() * radius
+    val dy = sin(angle).toFloat() * radius
+
+    val start = Offset(center.x - dx, center.y - dy)
+    val end = Offset(center.x + dx, center.y + dy)
+
+    return Brush.linearGradient(
+        colorStops =
+            arrayOf(
+                0.0f to Color(0xFFFF78C1),
+                0.509f to Color(0xFFE2017B),
+                1.0f to Color(0xFFAD005E),
+            ),
+        start = start,
+        end = end,
+    )
 }
--- androidApp/src/main/kotlin/com/yral/android/ui/widgets/YralButton.kt
+++ androidApp/src/main/kotlin/com/yral/android/ui/widgets/YralButton.kt
@@ -1,21 +1,24 @@
 package com.yral.android.ui.widgets
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.yral.android.ui.design.LocalAppTopography
 import com.yral.android.ui.design.YralColors
+import com.yral.android.ui.design.pinkGradient
 
 @Composable
 fun YralButton(
@@ -34,10 +37,20 @@ fun YralButton(
                     bottom = 12.dp,
                 ).fillMaxWidth()
                 .height(45.dp)
-                .background(
-                    color = getBackGroundColor(buttonType, buttonState),
-                    shape = RoundedCornerShape(8.dp),
-                ).clickable { onClick() },
+                .drawWithCache {
+                    val gradient =
+                        getBackGroundBrush(
+                            buttonType = buttonType,
+                            buttonState = buttonState,
+                            size = size,
+                        )
+                    onDrawBehind {
+                        drawRoundRect(
+                            brush = gradient,
+                            cornerRadius = CornerRadius(8.dp.toPx(), 8.dp.toPx()),
+                        )
+                    }
+                }.clickable { onClick() },
         horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterHorizontally),
         verticalAlignment = Alignment.CenterVertically,
     ) {
@@ -80,22 +93,23 @@ private fun getTextColor(
             }
     }
 
-private fun getBackGroundColor(
+private fun getBackGroundBrush(
     buttonType: YralButtonType,
     buttonState: YralButtonState,
-): Color =
+    size: Size,
+): Brush =
     when (buttonType) {
         YralButtonType.Pink ->
             when (buttonState) {
-                YralButtonState.Enabled -> YralColors.Pink300
-                YralButtonState.Disabled -> YralColors.Pink300
-                YralButtonState.Loading -> YralColors.Pink300
+                YralButtonState.Enabled -> pinkGradient(size)
+                YralButtonState.Disabled -> pinkGradient(size)
+                YralButtonState.Loading -> pinkGradient(size)
             }
 
         YralButtonType.White ->
             when (buttonState) {
-                YralButtonState.Enabled -> YralColors.Neutral50
-                YralButtonState.Disabled -> YralColors.Neutral50
-                YralButtonState.Loading -> YralColors.Neutral50
+                YralButtonState.Enabled -> pinkGradient(size)
+                YralButtonState.Disabled -> pinkGradient(size)
+                YralButtonState.Loading -> pinkGradient(size)
             }
     }
