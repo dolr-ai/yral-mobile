256b41b42fb8072cfead681c5352067b82128148
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-05-06 14:44:22 +0530

    build: Adds signup failed bottomSheet

--- androidApp/src/main/kotlin/com/yral/android/ui/screens/RootScreen.kt
+++ androidApp/src/main/kotlin/com/yral/android/ui/screens/RootScreen.kt
@@ -6,8 +6,18 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -16,6 +26,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.airbnb.lottie.compose.LottieAnimation
@@ -24,11 +37,15 @@ import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.yral.android.R
+import com.yral.android.ui.design.LocalAppTopography
+import com.yral.android.ui.design.YralColors
 import com.yral.android.ui.screens.home.HomeScreen
+import com.yral.android.ui.widgets.YralGradientButton
 import com.yral.android.ui.widgets.YralLoader
 import com.yral.shared.features.root.viewmodels.RootViewModel
 import org.koin.compose.viewmodel.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RootScreen(viewModel: RootViewModel = koinViewModel()) {
     val state by viewModel.state.collectAsState()
@@ -79,6 +96,15 @@ fun RootScreen(viewModel: RootViewModel = koinViewModel()) {
             ) {
                 YralLoader()
             }
+        }
+        if (state.showSignupFailedBottomSheet) {
+            SingUpFailedBottomSheet(
+                bottomSheetState =
+                    rememberModalBottomSheetState(
+                        skipPartiallyExpanded = true,
+                    ),
+                onDismissRequest = { viewModel.setShowSignupFailedBottomSheet(false) },
+            )
         }
     }
 }
@@ -139,4 +165,59 @@ private fun Splash(
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SingUpFailedBottomSheet(
+    bottomSheetState: SheetState,
+    onDismissRequest: () -> Unit,
+) {
+    ModalBottomSheet(
+        modifier = Modifier.safeDrawingPadding(),
+        onDismissRequest = onDismissRequest,
+        sheetState = bottomSheetState,
+        containerColor = YralColors.Neutral900,
+        dragHandle = null,
+        content = {
+            Column(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            start = 16.dp,
+                            top = 24.dp,
+                            end = 16.dp,
+                            bottom = 36.dp,
+                        ),
+                verticalArrangement = Arrangement.spacedBy(32.dp, Alignment.Top),
+                horizontalAlignment = Alignment.Start,
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.Top),
+                    horizontalAlignment = Alignment.Start,
+                ) {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = stringResource(R.string.could_not_login),
+                        style = LocalAppTopography.current.xlSemiBold,
+                        textAlign = TextAlign.Center,
+                        color = Color.White,
+                    )
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = stringResource(R.string.could_not_login_desc),
+                        style = LocalAppTopography.current.regRegular,
+                        textAlign = TextAlign.Center,
+                        color = Color.White,
+                    )
+                }
+                YralGradientButton(
+                    text = stringResource(R.string.ok),
+                    onClick = onDismissRequest,
+                )
+            }
+        },
+    )
 }
