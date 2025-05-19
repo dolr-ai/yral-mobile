69d6db4d83f77d338aa1d87e470373cf9b279695
samarth-paboowal-yral <samarth@gobazzinga.io>
2025-05-19 14:20:17 +0530

    build: adds use case for updating principal to firebase

---
+++ iosApp/iosApp/SmileyGame/Data/ExchangePrincipal.swift
@@ -0,0 +1,23 @@
+//
+//  ExchangePrincipal.swift
+//  iosApp
+//
+//  Created by Samarth Paboowal on 19/05/25.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import Foundation
+
+struct ExchangePrincipalDTO: Decodable {
+  let token: String
+  let coins: Int
+}
+
+extension ExchangePrincipalDTO {
+  func toDomain() -> ExchangePrincipalResponse {
+    return ExchangePrincipalResponse(
+      token: token,
+      coins: coins
+    )
+  }
+}
