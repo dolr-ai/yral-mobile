9be11f6fdc03407ee3c3e14fbda93c53a3bb81e7
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2025-05-21 13:11:57 +0530

    Implements proguard and disables logs for release build (#201)
    * build: Implements proguard for release builds
    
    * build: Disables rust logger for release builds
    
    * build: Disables KermitLogger logger for release builds
    
    * chore: Optimises extra configurations
    
    * chore: Disables R8 redundant flag for fullMode
    
    * chore: Removes redundant proguard rules for kotlinx serialization

--- shared/core/src/commonMain/kotlin/com/yral/shared/core/di/CoreModule.kt
+++ shared/core/src/commonMain/kotlin/com/yral/shared/core/di/CoreModule.kt
@@ -1,7 +1,6 @@
 package com.yral.shared.core.di
 
 import com.yral.shared.core.dispatchers.AppDispatchers
-import com.yral.shared.core.logging.YralLogger
 import com.yral.shared.core.platform.PlatformResourcesFactory
 import com.yral.shared.core.session.SessionManager
 import org.koin.dsl.module
@@ -11,5 +10,4 @@ val coreModule =
         single { PlatformResourcesFactory() }
         single { AppDispatchers() }
         single { SessionManager() }
-        single { YralLogger(get()) }
     }
