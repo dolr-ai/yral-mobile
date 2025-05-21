afe5f31411fc5d8bd641ab93b8dedf03fc2ee40a
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-05-21 16:01:18 +0530

    build: Implements game repository and remoteDataSource with dummy data

---
+++ shared/features/game/src/commonMain/kotlin/com/yral/shared/features/game/data/models/GameConfigDto.kt
@@ -0,0 +1,42 @@
+package com.yral.shared.features.game.data.models
+
+import com.yral.shared.features.game.domain.models.GameConfig
+import com.yral.shared.features.game.domain.models.GameIcon
+import com.yral.shared.features.game.domain.models.GameIconNames
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GameConfigDto(
+    @SerialName("available_smileys")
+    val availableSmileys: List<GameIconDto>,
+    @SerialName("loss_penalty")
+    val lossPenalty: Int,
+)
+
+@Serializable
+data class GameIconDto(
+    val id: String,
+    @SerialName("image_name")
+    val imageName: String,
+    @SerialName("is_active")
+    val isActive: Boolean,
+)
+
+fun GameConfigDto.toGameConfig(): GameConfig =
+    GameConfig(
+        lossPenalty = lossPenalty,
+        availableSmileys =
+            availableSmileys
+                .filter { it.isActive }
+                .map { it.toGameIcon() },
+    )
+
+fun GameIconDto.toGameIcon(): GameIcon =
+    GameIcon(
+        id = id,
+        imageName =
+            GameIconNames.valueOf(
+                imageName.uppercase(),
+            ),
+    )
