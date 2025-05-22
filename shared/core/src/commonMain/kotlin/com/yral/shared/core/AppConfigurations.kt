c9b809c5fde72029b4def114bb90404a3d1bb588
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-05-22 13:28:34 +0530

    build: Implements UpdateSessionAsRegistered api

--- shared/core/src/commonMain/kotlin/com/yral/shared/core/AppConfigurations.kt
+++ shared/core/src/commonMain/kotlin/com/yral/shared/core/AppConfigurations.kt
@@ -3,6 +3,7 @@ package com.yral.shared.core
 object AppConfigurations {
     const val ANONYMOUS_IDENTITY_BASE_URL = "yral.com"
     const val OAUTH_BASE_URL = "auth.yral.com"
+    const val METADATA_BASE_URL = "yral-metadata.fly.dev"
     const val FEED_BASE_URL = "yral-ml-feed-server.fly.dev"
     const val OFF_CHAIN_BASE_URL = "icp-off-chain-agent.fly.dev"
 }
