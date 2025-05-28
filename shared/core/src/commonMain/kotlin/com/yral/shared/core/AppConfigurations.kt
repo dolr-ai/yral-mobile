124f7105b56d7df62eb77c890853d9781d4f0f15
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2025-05-28 22:06:09 +0530

    Handles oAuth changes of id_token instead of access_token (#213)
    * build: Handles oauth changes of id_token instead of access_token
    
    * build: Updates oAuth base url
    
    * build: Implements UpdateSessionAsRegistered api
    
    * chore: Improves DI for auth
    
    * fix: Fixes authorization header
    
    * fix: Fixes coroutine scope to handle failures properly
    
    * fix: Fixes coroutine scope to handle failures properly in viewModels
    
    * build: Updates android terms of service

--- shared/core/src/commonMain/kotlin/com/yral/shared/core/AppConfigurations.kt
+++ shared/core/src/commonMain/kotlin/com/yral/shared/core/AppConfigurations.kt
@@ -2,7 +2,8 @@ package com.yral.shared.core
 
 object AppConfigurations {
     const val ANONYMOUS_IDENTITY_BASE_URL = "yral.com"
-    const val OAUTH_BASE_URL = "yral-auth-v2.fly.dev"
+    const val OAUTH_BASE_URL = "auth.yral.com"
+    const val METADATA_BASE_URL = "yral-metadata.fly.dev"
     const val FEED_BASE_URL = "yral-ml-feed-server.fly.dev"
     const val OFF_CHAIN_BASE_URL = "icp-off-chain-agent.fly.dev"
 }
