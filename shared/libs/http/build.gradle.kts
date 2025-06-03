b3171e180c77d7f7989f0acc0ba024961dc7b166
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2025-06-03 15:28:14 +0530

    Changes http client (#242)
    * fix: Changes exception from IllegalException to YralException
    
    * fix: Changes Http client from CIO to OkHttp for android
    
    * fix: Updates github action to remove changesNotSentForReview
    
    * build: Locks screen orientation to portrait

--- shared/libs/http/build.gradle.kts
+++ shared/libs/http/build.gradle.kts
@@ -26,10 +26,10 @@ kotlin {
 
     sourceSets {
         androidMain.dependencies {
+            api(libs.ktor.client.okhttp)
         }
         commonMain.dependencies {
             api(libs.ktor.client.core)
-            api(libs.ktor.client.cio)
             api(libs.ktor.client.logging)
             api(libs.ktor.client.content.negotiation)
             api(libs.ktor.json)
