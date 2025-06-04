d748198a1282440a28ed9acc9001665fb61b94b2
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-06-04 16:30:01 +0530

    build: Optimises DI for game module

--- shared/features/game/src/commonMain/kotlin/com/yral/shared/features/game/di/GameModule.kt
+++ shared/features/game/src/commonMain/kotlin/com/yral/shared/features/game/di/GameModule.kt
@@ -8,15 +8,15 @@ import com.yral.shared.features.game.domain.GetGameRulesUseCase
 import com.yral.shared.features.game.domain.IGameRepository
 import com.yral.shared.features.game.viewmodel.GameViewModel
 import org.koin.core.module.dsl.bind
-import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val gameModule =
     module {
-        singleOf(::GetGameIconsUseCase)
-        singleOf(::GetGameRulesUseCase)
+        factoryOf(::GetGameIconsUseCase)
+        factoryOf(::GetGameRulesUseCase)
         viewModelOf(::GameViewModel)
-        singleOf(::GameRepository) { bind<IGameRepository>() }
-        singleOf(::GameRemoteDataSource) { bind<IGameRemoteDataSource>() }
+        factoryOf(::GameRepository) { bind<IGameRepository>() }
+        factoryOf(::GameRemoteDataSource) { bind<IGameRemoteDataSource>() }
     }
