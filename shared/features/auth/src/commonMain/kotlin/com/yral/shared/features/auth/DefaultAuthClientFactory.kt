70fdcc5749abb7d341dc17e5c78ee2f500ceb069
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-06-27 16:46:42 +0530

    code review: Updates Cancellation exception handling in DefaultAuthClient exceptionHandler

--- shared/features/auth/src/commonMain/kotlin/com/yral/shared/features/auth/DefaultAuthClientFactory.kt
+++ shared/features/auth/src/commonMain/kotlin/com/yral/shared/features/auth/DefaultAuthClientFactory.kt
@@ -6,6 +6,7 @@ import com.yral.shared.crashlytics.core.CrashlyticsManager
 import com.yral.shared.features.auth.domain.AuthRepository
 import com.yral.shared.features.auth.utils.OAuthUtils
 import com.yral.shared.preferences.Preferences
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.plus
@@ -34,10 +35,18 @@ class DefaultAuthClientFactory(
             scope =
                 scope +
                     CoroutineExceptionHandler { _, throwable ->
-                        if (throwable is YralAuthException) {
-                            onAuthError(throwable)
-                        } else {
-                            onAuthError(YralAuthException("Unknown - ${throwable.message}"))
+                        when (throwable) {
+                            is CancellationException -> {
+                                throw throwable
+                            }
+
+                            is YralAuthException -> {
+                                onAuthError(throwable)
+                            }
+
+                            else -> {
+                                onAuthError(YralAuthException("Unknown - ${throwable.message}"))
+                            }
                         }
                     },
         )
