bd04cf3d2c04b86c4f9d13d85eea07af1433853a
mayank-k-yral <mayank@gobazzinga.io>
2025-06-30 00:47:49 +0530

    Add coroutines-x lib module

--- shared/core/src/commonMain/kotlin/com/yral/shared/core/dispatchers/AppDispatchers.kt
+++ shared/core/src/commonMain/kotlin/com/yral/shared/core/dispatchers/AppDispatchers.kt
@@ -3,6 +3,7 @@ package com.yral.shared.core.dispatchers
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
+@Deprecated("use AppDispatchers from coroutines-x")
 class AppDispatchers(
     val main: CoroutineDispatcher = Dispatchers.Main,
     val io: CoroutineDispatcher = provideIODispatcher(),
