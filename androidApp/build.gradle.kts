3a6e5044a74c5153710035efcb82a0d71ae7be07
mayank-k-yral <mayank@gobazzinga.io>
2025-07-17 22:03:12 +0530

    build: Adds firebase messaging to Android app

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -83,6 +83,7 @@ dependencies {
     implementation(libs.firebase.performance)
     implementation(libs.firebase.appcheck.playintegrity)
     implementation(libs.firebase.appcheck.debug)
+    implementation(libs.firebase.messaging)
     implementation(libs.lottie)
     implementation(libs.coil.compose)
     implementation(libs.coil.okhttp)
