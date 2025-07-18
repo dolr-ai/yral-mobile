97ddaddbc5425ffb92cc044e32f9b8ff049cf166
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2025-07-18 20:18:47 +0530

    Adds gradle convention plugin for rust lib (#339)

--- shared/features/uploadvideo/build.gradle.kts
+++ shared/features/uploadvideo/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.yral.shared.feature)
     alias(libs.plugins.yral.android.feature)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.yral.shared.rust.lib)
 }
 
 kotlin {
@@ -24,10 +25,6 @@ kotlin {
             implementation(projects.shared.libs.useCase)
             implementation(projects.shared.libs.arch)
             implementation(projects.shared.libs.coroutinesX)
-
-            val (deps, addRust) = BuildConfig.getAndProcessDependencies(project)
-            deps.forEach { if (it.isNotEmpty()) implementation(it) }
-            if (addRust) implementation(projects.shared.rust)
         }
     }
 }
