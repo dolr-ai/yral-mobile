40be6c96b6290c1e265ef1e51278cb1f44991b60
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-07-24 18:28:09 +0530

    code review: Adds try catch for haptics and sound

--- androidApp/src/main/kotlin/com/yral/android/ui/widgets/YralFeedback.kt
+++ androidApp/src/main/kotlin/com/yral/android/ui/widgets/YralFeedback.kt
@@ -8,10 +8,14 @@ import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
 import co.touchlab.kermit.Logger
+import com.yral.shared.core.exceptions.YralException
+import com.yral.shared.crashlytics.core.CrashlyticsManager
 import kotlinx.coroutines.delay
+import org.koin.compose.koinInject
 
 private const val HAPTIC_FEEDBACK_DELAY = 30L
 
+@Suppress("TooGenericExceptionCaught")
 @Composable
 fun YralFeedback(
     sound: Int,
@@ -21,24 +25,38 @@ fun YralFeedback(
 ) {
     val context = LocalContext.current
     val haptic = LocalHapticFeedback.current
+    val crashlyticsManager = koinInject<CrashlyticsManager>()
     DisposableEffect(Unit) {
+        var mediaPlayer: MediaPlayer? = null
         Logger.d("SoundAndHaptics") { "Playing $sound" }
-        val mediaPlayer = MediaPlayer.create(context, sound)
-        mediaPlayer.start()
-        mediaPlayer.setOnCompletionListener { onPlayed() }
+        try {
+            mediaPlayer = MediaPlayer.create(context, sound)
+            mediaPlayer.start()
+            mediaPlayer.setOnCompletionListener { onPlayed() }
+        } catch (e: Exception) {
+            crashlyticsManager.recordException(YralException("Error in dispatching sound $e"))
+        }
         onDispose {
-            mediaPlayer.release()
+            try {
+                mediaPlayer?.release()
+            } catch (e: Exception) {
+                crashlyticsManager.recordException(YralException("Error in releasing media player $e"))
+            }
         }
     }
     LaunchedEffect(Unit) {
         Logger.d("SoundAndHaptics") { "Playing $hapticFeedbackType" }
-        val hapticDuration = hapticFeedbackType.extendedHapticDuration()
-        if (withHapticFeedback) {
-            val startTime = System.currentTimeMillis()
-            while (System.currentTimeMillis() - startTime < hapticDuration) {
-                haptic.performHapticFeedback(hapticFeedbackType)
-                delay(HAPTIC_FEEDBACK_DELAY)
+        try {
+            val hapticDuration = hapticFeedbackType.extendedHapticDuration()
+            if (withHapticFeedback) {
+                val startTime = System.currentTimeMillis()
+                while (System.currentTimeMillis() - startTime < hapticDuration) {
+                    haptic.performHapticFeedback(hapticFeedbackType)
+                    delay(HAPTIC_FEEDBACK_DELAY)
+                }
             }
+        } catch (e: Exception) {
+            crashlyticsManager.recordException(YralException("Error in dispatching haptics $e"))
         }
     }
 }
