6aeca45a2394663c71e35a5f4d02aaee9acbe067
mayank-k-yral <mayank@gobazzinga.io>
2025-07-28 01:07:52 +0530

    AutoRechargeBalance API integration

--- shared/features/game/src/commonMain/kotlin/com/yral/shared/features/game/data/GameRemoteDataSource.kt
+++ shared/features/game/src/commonMain/kotlin/com/yral/shared/features/game/data/GameRemoteDataSource.kt
@@ -2,6 +2,8 @@ package com.yral.shared.features.game.data
 
 import com.yral.shared.core.AppConfigurations.PUMP_DUMP_BASE_URL
 import com.yral.shared.core.exceptions.YralException
+import com.yral.shared.features.game.data.models.AutoRechargeBalanceRequestDto
+import com.yral.shared.features.game.data.models.AutoRechargeBalanceResponseDto
 import com.yral.shared.features.game.data.models.CastVoteRequestDto
 import com.yral.shared.features.game.data.models.CastVoteResponseDto
 import com.yral.shared.features.game.data.models.GetBalanceResponseDto
@@ -61,8 +63,38 @@ class GameRemoteDataSource(
             }
         }
 
+    @Suppress("SwallowedException", "TooGenericExceptionCaught")
+    override suspend fun autoRechargeBalance(
+        idToken: String,
+        request: AutoRechargeBalanceRequestDto,
+    ): AutoRechargeBalanceResponseDto {
+        try {
+            val response: HttpResponse =
+                httpClient.post {
+                    expectSuccess = false
+                    url {
+                        host = cloudFunctionUrl()
+                        path(AUTO_RECHARGE_BALANCE_PATH)
+                    }
+                    headers.append("authorization", "Bearer $idToken")
+                    setBody(request)
+                }
+            val apiResponseString = response.bodyAsText()
+            val responseDto =
+                if (response.status == HttpStatusCode.OK) {
+                    json.decodeFromString<AutoRechargeBalanceResponseDto.Success>(apiResponseString)
+                } else {
+                    json.decodeFromString<AutoRechargeBalanceResponseDto.Error>(apiResponseString)
+                }
+            return responseDto
+        } catch (e: Exception) {
+            throw YralException("Error in updating balance: ${e.message}")
+        }
+    }
+
     companion object {
         private const val CAST_VOTE_PATH = "cast_vote"
         private const val GET_BALANCE_PATH = "v2/balance"
+        private const val AUTO_RECHARGE_BALANCE_PATH = "tap_to_recharge"
     }
 }
