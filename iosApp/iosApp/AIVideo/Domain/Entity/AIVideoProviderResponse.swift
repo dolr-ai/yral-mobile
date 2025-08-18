5958f47498a90927ef5cc8f661b31f8feb397713
samarth-paboowal-yral <samarth@gobazzinga.io>
2025-08-18 18:53:16 +0530

    build: adds UI & API for AI models

--- iosApp/iosApp/AIVideo/Domain/Entity/AIVideoProviderResponse.swift
+++ iosApp/iosApp/AIVideo/Domain/Entity/AIVideoProviderResponse.swift
@@ -1,5 +1,5 @@
 //
-//  ModelOption.swift
+//  AIVideoProviderResponse.swift
 //  iosApp
 //
 //  Created by Samarth Paboowal on 13/08/25.
@@ -8,10 +8,15 @@
 
 import Foundation
 
-struct ModelOption {
+struct AIVideoProviderMetaResponse {
+  let providers: [AIVideoProviderResponse]
+}
+
+struct AIVideoProviderResponse: Identifiable {
   let id: String
   let name: String
   let description: String
   let isActive: Bool
   let iconURL: String
+  let defaultDuration: Int
 }
---
+++ iosApp/iosApp/AIVideo/Domain/Interface/AIVideoRepositoryProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  AIVideoRepositoryProtocol.swift
+//  iosApp
+//
+//  Created by Samarth Paboowal on 18/08/25.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import Foundation
+
+protocol AIVideoRepositoryProtocol {
+  func getProviders() async -> Result<AIVideoProviderMetaResponse, AIVideoProviderError>
+}
