af90cfa9b9f5f279dbef20f657d4729dd134ac0f
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-08-20 10:15:09 +0530

    build: Removes extra back handler

--- androidApp/src/main/kotlin/com/yral/android/ui/screens/uploadVideo/UploadVideoRootScreen.kt
+++ androidApp/src/main/kotlin/com/yral/android/ui/screens/uploadVideo/UploadVideoRootScreen.kt
@@ -1,14 +1,11 @@
 package com.yral.android.ui.screens.uploadVideo
 
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import com.arkivanov.decompose.extensions.compose.stack.Children
-import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import com.yral.android.ui.screens.uploadVideo.aiVideoGen.AiVideoGenScreen
 import com.yral.android.ui.screens.uploadVideo.fileUpload.UploadVideoScreen
 import com.yral.android.ui.screens.uploadVideo.flowSelection.FlowSelectionScreen
@@ -18,9 +15,6 @@ fun UploadVideoRootScreen(
     component: UploadVideoRootComponent,
     bottomPadding: Dp,
 ) {
-    val stack by component.stack.subscribeAsState()
-    val canPop = stack.items.size > 1
-    BackHandler(enabled = canPop, onBack = { component.onBackClicked() })
     Children(
         stack = component.stack,
         modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer),
