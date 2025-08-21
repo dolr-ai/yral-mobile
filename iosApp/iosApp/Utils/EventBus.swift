61ebaf69ea381624e50ece9fc06fae46f0e7077e
samarth-paboowal-yral <samarth@gobazzinga.io>
2025-08-21 14:19:32 +0530

    build: integrates old video upload flow

---
+++ iosApp/iosApp/Utils/EventBus.swift
@@ -0,0 +1,14 @@
+//
+//  EventBus.swift
+//  iosApp
+//
+//  Created by Samarth Paboowal on 21/08/25.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import Combine
+
+final class EventBus: ObservableObject {
+  let finishUploadingVideo = PassthroughSubject<Void, Never>()
+  let finishUploadingAIVideo = PassthroughSubject<Void, Never>()
+}
