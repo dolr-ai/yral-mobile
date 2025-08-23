71f56a175802abaa7ab9aa0503facaf5c2508aec
mayank-k-yral <mayank@gobazzinga.io>
2025-08-23 12:56:53 +0530

    Bump android to 1.5.0(10)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     namespace = "com.yral.android"
     defaultConfig {
         applicationId = "com.yral.android"
-        versionCode = 9
-        versionName = "1.4.1"
+        versionCode = 10
+        versionName = "1.5.0"
         ndkVersion = "28.0.13004108"
         buildConfigField(
             type = "String",
