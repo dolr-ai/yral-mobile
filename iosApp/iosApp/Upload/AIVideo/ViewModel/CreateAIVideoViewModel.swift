efd8cbdabe469318d0126d6e23a138b66e6bba1a
samarth-paboowal-yral <samarth@gobazzinga.io>
2025-08-29 16:50:38 +0530

    chore: updates local balance video generation

--- iosApp/iosApp/Upload/AIVideo/ViewModel/CreateAIVideoViewModel.swift
+++ iosApp/iosApp/Upload/AIVideo/ViewModel/CreateAIVideoViewModel.swift
@@ -35,7 +35,8 @@ enum CreateAIVideoScreenEvent {
   case socialSignInFailure
   case generateVideoSuccess(deductBalance: Int)
   case generateVideoFailure(String)
-  case generateVideoStatusFailure(error: String, addBalance: Int)
+  case generateVideoStatusSuccess(deductBalance: Int)
+  case generateVideoStatusFailure(String)
   case uploadAIVideoSuccess(String)
   case uploadAIVideoFailure(String)
 }
@@ -184,7 +185,14 @@ class CreateAIVideoViewModel: ObservableObject {
                 reasonType: nil
               )
             )
-            Task { await uploadAIVideo() }
+
+            if let provider = selectedProvider {
+              event = .generateVideoStatusSuccess(deductBalance: provider.cost.sats)
+            }
+
+            Task {
+              await uploadAIVideo()
+            }
           } else if status.contains("Failed: ") {
             stopPolling()
             AnalyticsModuleKt.getAnalyticsManager().trackEvent(
