37434c26b272400cb3af2788c69d0b98245bc3f1
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-09-15 12:33:40 +0530

    build: Integrates accounts link in upload video login

--- shared/features/uploadvideo/build.gradle.kts
+++ shared/features/uploadvideo/build.gradle.kts
@@ -23,6 +23,7 @@ kotlin {
             implementation(projects.shared.libs.http)
             implementation(projects.shared.libs.arch)
             implementation(projects.shared.libs.coroutinesX)
+            implementation(projects.shared.libs.featureFlag)
             implementation(projects.shared.rust.service)
         }
     }
