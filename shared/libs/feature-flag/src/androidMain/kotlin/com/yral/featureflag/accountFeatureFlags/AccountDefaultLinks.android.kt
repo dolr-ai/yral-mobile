21d2e0b25b8012bc58d5b0fdcc9f554257af23fa
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-09-15 12:22:28 +0530

    build: Introduces AccountLinks in remote config

---
+++ shared/libs/feature-flag/src/androidMain/kotlin/com/yral/featureflag/accountFeatureFlags/AccountDefaultLinks.android.kt
@@ -0,0 +1,16 @@
+package com.yral.featureflag.accountFeatureFlags
+
+internal actual object AccountDefaultLinks {
+    actual val TALK_TO_TEAM_URL: String
+        get() = "https://chat.whatsapp.com/LMLykiRUrJD13oUtM61JJX?mode=ems_share_t"
+    actual val TERMS_OF_SERVICE_URL: String
+        get() = "https://yral.com/terms-android"
+    actual val PRIVACY_POLICY_URL: String
+        get() = "https://yral.com/privacy-policy"
+    actual val TELEGRAM_LINK: String
+        get() = "https://t.me/+c-LTX0Cp-ENmMzI1"
+    actual val DISCORD_LINK: String
+        get() = "https://discord.com/invite/GZ9QemnZuj"
+    actual val TWITTER_LINK: String
+        get() = "https://twitter.com/Yral_app"
+}
