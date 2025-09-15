e270a948711440b0223688e1c599a95d4a83643f
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-09-15 14:24:10 +0530

    build: Bump android to 1.7.0(18)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     namespace = "com.yral.android"
     defaultConfig {
         applicationId = "com.yral.android"
-        versionCode = 17
+        versionCode = 18
         versionName = "1.7.0"
         ndkVersion = "28.0.13004108"
         buildConfigField(
