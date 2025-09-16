21331e01f70a0812603bdf4415bf74eb52d64d64
samarth-paboowal-yral <samarth@gobazzinga.io>
2025-09-16 13:57:37 +0530

    build: added constants related to award based leaderboard

---
+++ functions/constants.py
@@ -0,0 +1,22 @@
+SUPPORTED_REWARD_CURRENCIES = ["YRAL", "BTC"]
+DEFAULT_REWARD_CURRENCY = SUPPORTED_REWARD_CURRENCIES[0]
+
+REWARDS_ENABLED = True
+
+REWARD_AMOUNT = {
+    "YRAL": {
+        1: 100,
+        2: 80,
+        3: 50,
+        4: 40,
+        5: 20
+    },
+
+    "BTC": {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    }
+}
