1d5df768201533990e413a1f21ac41c9ff2c414c
samarth-paboowal-yral <samarth@gobazzinga.io>
2025-09-24 20:58:26 +0530

    chore: updates constants file to support INR & USD for BTC

--- functions/constants.py
+++ functions/constants.py
@@ -1,22 +1,34 @@
-SUPPORTED_REWARD_CURRENCIES = ["YRAL", "BTC"]
+CURRENCY_YRAL = "YRAL"
+CURRENCY_BTC = "BTC"
+COUNTRY_CODE_INDIA = "IN"
+COUNTRY_CODE_USA = "US"
+SUPPORTED_REWARD_CURRENCIES = [CURRENCY_YRAL, CURRENCY_BTC]
 DEFAULT_REWARD_CURRENCY = SUPPORTED_REWARD_CURRENCIES[0]
 
 REWARDS_ENABLED = True
 
 REWARD_AMOUNT = {
-    "YRAL": {
-        1: 100,
-        2: 80,
-        3: 50,
-        4: 40,
-        5: 20
+    CURRENCY_YRAL: {
+        "1": 100,
+        "2": 80,
+        "3": 50,
+        "4": 40,
+        "5": 20,
     },
-
-    "BTC": {
-        1: 100,
-        2: 80,
-        3: 50,
-        4: 40,
-        5: 20
+    CURRENCY_BTC: {
+        COUNTRY_CODE_INDIA: {
+            "1": 100,
+            "2": 80,
+            "3": 50,
+            "4": 40,
+            "5": 20,
+        },
+        COUNTRY_CODE_USA: {
+            "1": 5,
+            "2": 4,
+            "3": 3,
+            "4": 2,
+            "5": 1,
+        }
     }
 }
