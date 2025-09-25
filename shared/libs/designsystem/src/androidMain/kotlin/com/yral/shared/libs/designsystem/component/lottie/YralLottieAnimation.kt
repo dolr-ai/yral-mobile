76c3b5ef5caf1897c77e7d1c6f500ff88544a4c3
mayank-k-yral <mayank@gobazzinga.io>
2025-09-25 10:29:19 +0530

    Crash Fix: Lottie file names and loading logic (#514)
    - Renames `claim_unsucessful_wo_loading.lottie` to `claim_unsuccessful_wo_loading.lottie`.
    - Updates `LottieRes` to use file extensions and provides an `assetPath` for loading.
    - Simplifies `rememberYralLottieComposition` on Android to directly use `LottieCompositionSpec.Asset` with the new `assetPath`.

--- shared/libs/designsystem/src/androidMain/kotlin/com/yral/shared/libs/designsystem/component/lottie/YralLottieAnimation.kt
+++ shared/libs/designsystem/src/androidMain/kotlin/com/yral/shared/libs/designsystem/component/lottie/YralLottieAnimation.kt
@@ -4,9 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import com.airbnb.lottie.LottieComposition
@@ -14,7 +11,6 @@ import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
-import yral_mobile.shared.libs.designsystem.generated.resources.Res
 
 @Composable
 actual fun YralLottieAnimation(
@@ -35,18 +31,10 @@ actual fun YralLottieAnimation(
 }
 
 @Composable
-actual fun rememberYralLottieComposition(lottieRes: LottieRes): State<Any?> {
-    var bytes by remember {
-        mutableStateOf(ByteArray(0))
-    }
-    LaunchedEffect(Unit) {
-        bytes = Res.readBytes(lottieRes.path)
-    }
-    return rememberLottieComposition(
-        LottieCompositionSpec.JsonString(bytes.decodeToString()),
-//        LottieCompositionSpec.RawRes(R.raw.claim_successful_wo_loading),
+actual fun rememberYralLottieComposition(lottieRes: LottieRes): State<Any?> =
+    rememberLottieComposition(
+        LottieCompositionSpec.Asset(lottieRes.assetPath),
     )
-}
 
 @Composable
 actual fun YralLottieAnimation(
--- shared/libs/designsystem/src/commonMain/kotlin/com/yral/shared/libs/designsystem/component/lottie/LottieRes.kt
+++ shared/libs/designsystem/src/commonMain/kotlin/com/yral/shared/libs/designsystem/component/lottie/LottieRes.kt
@@ -1,33 +1,36 @@
 package com.yral.shared.libs.designsystem.component.lottie
 
+import yral_mobile.shared.libs.designsystem.generated.resources.Res
+
 @Suppress("ForbiddenComment")
 enum class LottieRes(
     filename: String,
 ) {
     // TODO: Move lottie to feature folders
-    LOADING("loading"),
-    SPLASH("splash_lottie"),
-    LIGHTNING("lightning_lottie"),
-    CLAIM_SUCCESSFUL_WO_LOADING("claim_successful_wo_loading"),
-    CLAIM_UNSUCCESSFUL_WO_LOADING("claim_unsuccessful_wo_loading"),
-    COMMON_LOADING("common_loading"),
-    SIGNUP_SCROLL("signup_scroll"),
-    SMILEY_GAME_FIRE("smiley_game_fire"),
-    SMILEY_GAME_HEART("smiley_game_heart"),
-    SMILEY_GAME_LAUGH("smiley_game_laugh"),
-    SMILEY_GAME_LOSE("smiley_game_lose"),
-    SMILEY_GAME_PUKE("smiley_game_puke"),
-    SMILEY_GAME_ROCKET("smiley_game_rocket"),
-    SMILEY_GAME_SURPRISE("smiley_game_surprise"),
-    SMILEY_GAME_WIN("smiley_game_win"),
-    COLORFUL_CONFETTI_BRUST("colorful_confetti_brust"),
-    LEADERBOARD_STAR("leaderboard_star"),
-    YELLOW_RAYS("yellow_rays"),
-    PURPLE_RAYS("purple_rays"),
-    YRAL_LOADER("yral_loader"),
-    WHITE_LOADER("white_loader"),
-    READ_LOADER("read_loader"),
+    SPLASH("splash_lottie.json"),
+    LIGHTNING("lightning_lottie.json"),
+    CLAIM_SUCCESSFUL_WO_LOADING("claim_successful_wo_loading.lottie"),
+    CLAIM_UNSUCCESSFUL_WO_LOADING("claim_unsuccessful_wo_loading.lottie"),
+    COMMON_LOADING("common_loading.lottie"),
+    SIGNUP_SCROLL("signup_scroll.json"),
+    SMILEY_GAME_FIRE("smiley_game_fire.json"),
+    SMILEY_GAME_HEART("smiley_game_heart.json"),
+    SMILEY_GAME_LAUGH("smiley_game_laugh.json"),
+    SMILEY_GAME_LOSE("smiley_game_lose.json"),
+    SMILEY_GAME_PUKE("smiley_game_puke.json"),
+    SMILEY_GAME_ROCKET("smiley_game_rocket.json"),
+    SMILEY_GAME_SURPRISE("smiley_game_surprise.json"),
+    SMILEY_GAME_WIN("smiley_game_win.json"),
+    COLORFUL_CONFETTI_BRUST("colorful_confetti_brust.json"),
+    LEADERBOARD_STAR("leaderboard_star.json"),
+    YELLOW_RAYS("yellow_rays.json"),
+    PURPLE_RAYS("purple_rays.json"),
+    YRAL_LOADER("yral_loader.json"),
+    WHITE_LOADER("white_loader.json"),
+    READ_LOADER("read_loader.json"),
     ;
 
-    val path: String = "files/lottie/$filename.json"
+    private val path: String = "files/lottie/$filename"
+
+    val assetPath = Res.getUri(path).removePrefix("file:///android_asset/")
 }
