ede51940acfcdbc1150e57446ef4cb028d611143
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-10-01 21:21:38 +0530

    build: Updates MyFirebaseMessagingService to handle rewards notification correctly

--- androidApp/src/main/kotlin/com/yral/android/MyFirebaseMessagingService.kt
+++ androidApp/src/main/kotlin/com/yral/android/MyFirebaseMessagingService.kt
@@ -2,18 +2,17 @@ package com.yral.android
 
 import android.content.Intent
 import androidx.annotation.WorkerThread
-import androidx.core.net.toUri
 import co.touchlab.kermit.Logger
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import com.yral.featureflag.FeatureFlagManager
-import com.yral.featureflag.WalletFeatureFlags
 import com.yral.shared.features.auth.domain.useCases.RegisterNotificationTokenUseCase
-import com.yral.shared.koin.koinInstance
 import com.yral.shared.libs.designsystem.component.toast.ToastManager
 import com.yral.shared.libs.designsystem.component.toast.ToastStatus
 import com.yral.shared.libs.designsystem.component.toast.ToastType
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import org.koin.android.ext.android.inject
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
@@ -39,14 +38,15 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     }
 
     private fun showToastForForegroundMessage(message: RemoteMessage) {
-        val notification = message.notification
-        notification?.let {
-            val data = message.data
-            if (data.contains("rewards_received_bs") && data["rewards_received_bs"].toBoolean()) {
-                handleBtcRewardsNotification(notification)
-            } else {
-                handleToastNotification(notification)
-            }
+        message.notification?.let { notification ->
+            message.data["payload"]?.let { payload ->
+                val jsonObject = Json.decodeFromString(JsonObject.serializer(), payload)
+                val type = jsonObject["type"]?.jsonPrimitive?.content
+                when (type) {
+                    "RewardEarned" -> handleBtcRewardsNotification(message)
+                    else -> handleToastNotification(notification)
+                }
+            } ?: handleToastNotification(notification)
         }
     }
 
@@ -71,15 +71,11 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         )
     }
 
-    private fun handleBtcRewardsNotification(notification: RemoteMessage.Notification) {
-        val featureFlagManager: FeatureFlagManager = koinInstance.get()
-        val link =
-            notification.link ?: featureFlagManager.get(WalletFeatureFlags.Wallet.BtcRewardsLink).toUri()
+    private fun handleBtcRewardsNotification(message: RemoteMessage) {
         val intent = Intent(this, MainActivity::class.java)
         // Use these flags to bring an existing instance to the front or create a new one if needed
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP)
-        intent.data = link
-        intent.putExtra("branch_force_new_session", true)
+        intent.putExtra("payload", message.data["payload"])
         startActivity(intent)
     }
 
