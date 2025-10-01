ba80a8170e8b00afc16ae06542864147b67a757e
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-10-01 16:18:10 +0530

    build: Removes offset from golden text brush

--- shared/libs/designsystem/src/commonMain/kotlin/com/yral/shared/libs/designsystem/theme/YralBrushes.kt
+++ shared/libs/designsystem/src/commonMain/kotlin/com/yral/shared/libs/designsystem/theme/YralBrushes.kt
@@ -1,6 +1,5 @@
 package com.yral.shared.libs.designsystem.theme
 
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
@@ -9,7 +8,5 @@ object YralBrushes {
     val GoldenTextBrush =
         Brush.linearGradient(
             colors = listOf(Color(0xFFFFCC00), Color(0xFFDA8100)),
-            start = Offset(23.197f, -18.929f),
-            end = Offset(71.175f, -15.036f),
         )
 }
