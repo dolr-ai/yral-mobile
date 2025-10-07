fe641fff4298558908edcb81ab4f072fac38437b
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-10-08 02:50:36 +0530

    Bump android to 1.8.1(22)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     namespace = "com.yral.android"
     defaultConfig {
         applicationId = "com.yral.android"
-        versionCode = 21
-        versionName = "1.8.0"
+        versionCode = 22
+        versionName = "1.8.1"
         ndkVersion = "28.0.13004108"
         buildConfigField(
             type = "String",
