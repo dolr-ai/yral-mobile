6cae14324b3d6fdfd3ee1acdf0427a8471fc119a
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-10-15 16:24:31 +0530

    build: Changes default overlay type

--- shared/features/feed/src/commonMain/kotlin/com/yral/shared/features/feed/viewmodel/FeedViewModel.kt
+++ shared/features/feed/src/commonMain/kotlin/com/yral/shared/features/feed/viewmodel/FeedViewModel.kt
@@ -621,7 +621,7 @@ data class FeedState(
     val isDeeplinkFetching: Boolean = false,
     val reportSheetState: ReportSheetState = ReportSheetState.Closed,
     val showSignupFailedSheet: Boolean = false,
-    val overlayType: OverlayType = OverlayType.DEFAULT,
+    val overlayType: OverlayType = OverlayType.DAILY_RANK,
 )
 
 enum class OverlayType {
