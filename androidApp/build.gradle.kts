68f016c83010720d661c53aadea0498a079276df
Shivam Bhavsar <shivambhavsar@gobazzinga.io>
2025-10-15 19:57:35 +0530

    Bump android to 1.9.0(26)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     namespace = "com.yral.android"
     defaultConfig {
         applicationId = "com.yral.android"
-        versionCode = 25
+        versionCode = 26
         versionName = "1.9.0"
         ndkVersion = "28.0.13004108"
         buildConfigField(
