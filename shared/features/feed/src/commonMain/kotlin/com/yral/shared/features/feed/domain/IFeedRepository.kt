e118a394f0d0f754be86dca76b6741f80781377d
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2025-10-17 23:11:48 +0530

    Integrates ai video end point (#579)
    * build: Integrates AI video feed api
    
    * build: Integrates in FeedViewModel
    
    * build: Adds feed switching functionality
    
    * build: Adds feed switching functionality with trigger to refetch feed
    
    * build: Disables feed switch while post details are being fetched
    
    * build: Enables fetch ai video recursion if vote check to be enabled
    
    * code review:
    - Applies recursion end condition for fetchingAIVideos
    - Updates FeedToggle image size modifier
    - Updates toLowerCase function usage

--- shared/features/feed/src/commonMain/kotlin/com/yral/shared/features/feed/domain/IFeedRepository.kt
+++ shared/features/feed/src/commonMain/kotlin/com/yral/shared/features/feed/domain/IFeedRepository.kt
@@ -1,9 +1,11 @@
 package com.yral.shared.features.feed.domain
 
+import com.yral.shared.features.feed.domain.models.AIFeedRequest
 import com.yral.shared.features.feed.domain.models.FeedRequest
 import com.yral.shared.features.feed.domain.models.PostResponse
 
 interface IFeedRepository {
     suspend fun getInitialFeeds(feedRequest: FeedRequest): PostResponse
     suspend fun fetchMoreFeeds(feedRequest: FeedRequest): PostResponse
+    suspend fun fetchAIFeeds(feedRequest: AIFeedRequest): PostResponse
 }
