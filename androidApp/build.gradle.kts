cef623632de6cbd192d4fb0f64c0a7c4f2fec02a
sarveshsharma <sarvesh@gobazzinga.io>
2025-11-04 21:46:21 +0530

    chore: Increments version to 33

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     namespace = "com.yral.android"
     defaultConfig {
         applicationId = "com.yral.android"
-        versionCode = 32
+        versionCode = 33
         versionName = "2.0.4"
         ndkVersion = "28.0.13004108"
         buildConfigField(
