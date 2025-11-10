737d5caeffa87a00cd6c93bd8137afa1745c4ddd
samarth-paboowal-yral <samarth@gobazzinga.io>
2025-11-10 13:36:04 +0530

    build: enabled leaderboard rewards on production

--- functions/constants.py
+++ functions/constants.py
@@ -3,9 +3,11 @@ CURRENCY_BTC = "BTC"
 COUNTRY_CODE_INDIA = "IN"
 COUNTRY_CODE_USA = "US"
 SUPPORTED_REWARD_CURRENCIES = [CURRENCY_YRAL, CURRENCY_BTC]
-DEFAULT_REWARD_CURRENCY = SUPPORTED_REWARD_CURRENCIES[0]
+DEFAULT_REWARD_CURRENCY = SUPPORTED_REWARD_CURRENCIES[1]
 
-REWARDS_ENABLED = False
+# Disabled in staging
+# Enabled in production
+REWARDS_ENABLED = True
 
 REWARD_AMOUNT = {
     CURRENCY_YRAL: {
@@ -17,15 +19,15 @@ REWARD_AMOUNT = {
     },
     CURRENCY_BTC: {
         COUNTRY_CODE_INDIA: {
-            "1": 100,
-            "2": 80,
-            "3": 50,
-            "4": 40,
-            "5": 20,
+            "1": 1000,
+            "2": 500,
+            "3": 250,
+            "4": 150,
+            "5": 100,
         },
         COUNTRY_CODE_USA: {
-            "1": 5,
-            "2": 4,
+            "1": 10,
+            "2": 5,
             "3": 3,
             "4": 2,
             "5": 1,
