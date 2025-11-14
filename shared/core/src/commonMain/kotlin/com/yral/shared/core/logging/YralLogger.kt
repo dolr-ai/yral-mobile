be044ab67d973436df17b3cceeb559c1bd85bc54
sarvesh-sharma-yral <sarvesh@gobazzinga.io>
2025-11-14 22:40:42 +0530

    Feature/sentry (#651)
    * build: Adds sentry integration
    
    * build: Adds logs to crashreports as well
    
    * build: Adds logs to crashreports as well
    
    * chore: Addresses review comments
    
    * chore: Adds sentry environment variables
    
    * chore: Addresses review feedback
    
    * chore: Removes auto performance tracing

--- shared/core/src/commonMain/kotlin/com/yral/shared/core/logging/YralLogger.kt
+++ shared/core/src/commonMain/kotlin/com/yral/shared/core/logging/YralLogger.kt
@@ -2,14 +2,23 @@ package com.yral.shared.core.logging
 
 import co.touchlab.kermit.LogWriter
 import co.touchlab.kermit.Logger
+import co.touchlab.kermit.Logger.Companion.config
+import co.touchlab.kermit.Logger.Companion.tag
 import co.touchlab.kermit.StaticConfig
 
 class YralLogger(
-    writer: LogWriter?,
+    logWriters: List<LogWriter> = emptyList(),
 ) : Logger(
         tag = "YralApp",
-        config =
-            StaticConfig(
-                logWriterList = writer?.let { listOf(writer) } ?: emptyList(),
-            ),
-    )
+        config = StaticConfig(logWriterList = logWriters),
+    ) {
+    fun withAdditionalLogWriter(writer: LogWriter): Logger =
+        Logger(
+            tag = tag,
+            config =
+                StaticConfig(
+                    minSeverity = config.minSeverity,
+                    logWriterList = config.logWriterList + writer,
+                ),
+        )
+}
--- shared/libs/http/src/commonMain/kotlin/com/yral/shared/http/HttpLogger.kt
+++ shared/libs/http/src/commonMain/kotlin/com/yral/shared/http/HttpLogger.kt
@@ -1,14 +1,21 @@
 package com.yral.shared.http
 
+import co.touchlab.kermit.LogWriter
 import com.yral.shared.core.logging.YralLogger
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 
 class HttpLogger(
-    private val logger: YralLogger,
+    baseLogger: YralLogger,
+    additionalLogWriter: LogWriter?,
 ) : Logger {
+    private val logger =
+        (additionalLogWriter?.let { baseLogger.withAdditionalLogWriter(it) } ?: baseLogger)
+            .withTag("HTTP")
+
     override fun log(message: String) {
         logger.d("HTTP Client $message")
     }
+
     val logLevel = LogLevel.BODY // todo set log level based on debug/release flag
 }
