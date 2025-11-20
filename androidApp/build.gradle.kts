65756279a6e2cc1fe51198754fa5b073f280d640
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2025-11-20 18:19:57 +0530

    build: Increments build number for uploading app for test release

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     namespace = "com.yral.android"
     defaultConfig {
         applicationId = "com.yral.android"
-        versionCode = 37
-        versionName = "2.0.7"
+        versionCode = 43
+        versionName = "2.0.8"
         ndkVersion = "28.0.13004108"
         buildConfigField(
             type = "String",
