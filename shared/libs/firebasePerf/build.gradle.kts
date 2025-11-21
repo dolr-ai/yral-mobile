0774ba2cdce1843937339afb4233ff27fb0143f3
mayank-k-yral <mayank@gobazzinga.io>
2025-11-21 11:20:16 +0530

    Add Firebase Performance trace attribute support for iOS (#668)

--- shared/libs/firebasePerf/build.gradle.kts
+++ shared/libs/firebasePerf/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.yral.shared.library)
     alias(libs.plugins.yral.android.library)
+    alias(libs.plugins.kotlinCocoapods)
 }
 
 kotlin {
@@ -9,6 +10,19 @@ kotlin {
         iosArm64(),
         iosSimulatorArm64(),
     )
+
+    cocoapods {
+        version = "1.0"
+        summary = "Firebase Perf"
+        homepage = "https://github.com/dolr-ai/yral-mobile"
+        ios.deploymentTarget = "15.6"
+
+        noPodspec()
+
+        pod("FirebasePerformance") {
+            version = "11.14.0"
+        }
+    }
 
     sourceSets {
         commonMain.dependencies {
