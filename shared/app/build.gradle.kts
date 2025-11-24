a7cd771e759a0cd5dfee8dd6e975da455d8c391a
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2025-11-24 14:33:25 +0530

    build: Resolves dependency issues

--- shared/app/build.gradle.kts
+++ shared/app/build.gradle.kts
@@ -28,6 +28,7 @@ kotlin {
             implementation(projects.shared.libs.sharing)
             implementation(projects.shared.libs.designsystem)
             implementation(projects.shared.libs.videoPlayer)
+            implementation(projects.shared.libs.fileIo)
             implementation(projects.shared.data)
 
             // Routing framework dependencies
