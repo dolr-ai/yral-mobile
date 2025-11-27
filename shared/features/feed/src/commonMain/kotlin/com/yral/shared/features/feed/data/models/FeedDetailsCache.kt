de9c4da9f6199fb88d892bf9e81bf1d59aa652ec
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2025-11-27 13:40:33 +0530

    code review: Uses default serializer for Instant

--- shared/features/feed/src/commonMain/kotlin/com/yral/shared/features/feed/data/models/FeedDetailsCache.kt
+++ shared/features/feed/src/commonMain/kotlin/com/yral/shared/features/feed/data/models/FeedDetailsCache.kt
@@ -1,20 +1,13 @@
 package com.yral.shared.features.feed.data.models
 
 import com.yral.shared.data.domain.models.FeedDetails
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
 @OptIn(ExperimentalTime::class)
 @Serializable
 internal data class FeedDetailsCache(
-    @Serializable(with = InstantAsLongSerializer::class)
     val timestamp: Instant,
     val feedDetails: List<FeedDetailsForCache>,
 )
@@ -81,18 +74,3 @@ internal fun FeedDetails.toFeedDetailsForCache(): FeedDetailsForCache =
         isFromServiceCanister = isFromServiceCanister,
         userName = userName,
     )
-
-@Serializable
-@OptIn(ExperimentalTime::class)
-internal object InstantAsLongSerializer : KSerializer<Instant> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Instant", PrimitiveKind.LONG)
-
-    override fun serialize(
-        encoder: Encoder,
-        value: Instant,
-    ) {
-        encoder.encodeLong(value.toEpochMilliseconds())
-    }
-
-    override fun deserialize(decoder: Decoder): Instant = Instant.fromEpochMilliseconds(decoder.decodeLong())
-}
