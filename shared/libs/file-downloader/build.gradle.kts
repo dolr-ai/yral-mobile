8425d7ea0d3273e9056432a98c70a0e62cc6c1db
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2025-11-28 14:23:25 +0530

    Implements file-downloader module (#678)

---
+++ shared/libs/file-downloader/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    alias(libs.plugins.yral.shared.library)
+    alias(libs.plugins.yral.android.library)
+}
+
+kotlin {
+    androidTarget()
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64(),
+    )
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.shared.core)
+            implementation(projects.shared.libs.koin)
+            implementation(libs.touchlab.logger)
+            api(libs.kotlinResult.core)
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.ktor.client.core)
+            implementation(libs.kotlinx.io.core)
+        }
+        androidMain.dependencies {
+            implementation(libs.ktor.client.okhttp)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
+    }
+}
+
+android {
+    namespace = "com.yral.shared.libs.filedownloader"
+}
