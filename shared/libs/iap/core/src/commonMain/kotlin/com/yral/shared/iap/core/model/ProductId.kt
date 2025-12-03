6a93645ece542512e3599ebcfb37308cd2e9d71a
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2025-12-03 14:27:02 +0530

    code review: Adds serialName to ProductId enum

--- shared/libs/iap/core/src/commonMain/kotlin/com/yral/shared/iap/core/model/ProductId.kt
+++ shared/libs/iap/core/src/commonMain/kotlin/com/yral/shared/iap/core/model/ProductId.kt
@@ -1,13 +1,19 @@
 package com.yral.shared.iap.core.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 enum class ProductId(
     val productId: String,
 ) {
+    @SerialName("premium_monthly")
     PREMIUM_MONTHLY("premium_monthly"),
+
+    @SerialName("premium_yearly")
     PREMIUM_YEARLY("premium_yearly"),
+
+    @SerialName("remove_ads")
     REMOVE_ADS("remove_ads"),
     ;
 
