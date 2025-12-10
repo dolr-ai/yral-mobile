009eacb6192dc28c283d354ee7b107d97bd9c708
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2025-12-10 21:38:29 +0530

    Bump android version to 2.3.0(47)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     namespace = "com.yral.android"
     defaultConfig {
         applicationId = "com.yral.android"
-        versionCode = 46
-        versionName = "2.2.0"
+        versionCode = 47
+        versionName = "2.3.0"
         ndkVersion = "28.0.13004108"
         buildConfigField(
             type = "String",
