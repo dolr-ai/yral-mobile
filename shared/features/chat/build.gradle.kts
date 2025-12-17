44662a432611b8051843d7d0ad77674537c6c1a6
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2025-12-17 11:11:43 +0530

    build: Initiates chat module

---
+++ shared/features/chat/build.gradle.kts
@@ -0,0 +1,35 @@
+import org.gradle.kotlin.dsl.implementation
+
+plugins {
+    alias(libs.plugins.yral.shared.feature)
+    alias(libs.plugins.yral.android.feature)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+kotlin {
+    androidTarget()
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64(),
+    )
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.shared.core)
+            implementation(projects.shared.libs.koin)
+            implementation(projects.shared.libs.crashlytics)
+            implementation(projects.shared.libs.analytics)
+            implementation(projects.shared.libs.coroutinesX)
+            implementation(projects.shared.libs.featureFlag)
+            implementation(projects.shared.libs.preferences)
+
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.androidx.lifecycle.viewmodel)
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            implementation(libs.kotlinx.coroutines.test)
+            implementation(libs.turbine)
+        }
+    }
+}
