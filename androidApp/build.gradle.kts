082923b1630d5f543c484dce26d3ece41cd5e184
sarveshsharma <sarvesh@gobazzinga.io>
2025-12-19 17:41:35 +0000

    chore: Increases android app version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     namespace = "com.yral.android"
     defaultConfig {
         applicationId = "com.yral.android"
-        versionCode = 52
+        versionCode = 53
         versionName = "2.4.0"
         ndkVersion = "28.0.13004108"
         buildConfigField(
