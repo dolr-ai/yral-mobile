41d1943b715079cf3cb142f7053e4627533462e0
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2026-01-05 13:26:54 +0530

    build: Adds bottom coins in MandatoryLoginScreen.kt

--- shared/app/src/commonMain/kotlin/com/yral/shared/app/ui/screens/MandatoryLoginScreen.kt
+++ shared/app/src/commonMain/kotlin/com/yral/shared/app/ui/screens/MandatoryLoginScreen.kt
@@ -3,15 +3,19 @@ package com.yral.shared.app.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -21,10 +25,13 @@ import com.yral.shared.features.auth.ui.SignupView
 import com.yral.shared.features.auth.ui.getAnnotatedHeaderForLogin
 import com.yral.shared.features.auth.viewModel.LoginViewModel
 import com.yral.shared.libs.designsystem.component.YralWebViewBottomSheet
+import com.yral.shared.libs.designsystem.windowInfo.rememberScreenFoldStateProvider
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 import yral_mobile.shared.app.generated.resources.Res
+import yral_mobile.shared.app.generated.resources.login_bottom_shadow
+import yral_mobile.shared.app.generated.resources.login_coins
 import yral_mobile.shared.app.generated.resources.login_to_unlock_tokens
 import yral_mobile.shared.app.generated.resources.login_top_shadow
 import yral_mobile.shared.app.generated.resources.reward_tokens
@@ -48,15 +55,10 @@ fun MandatoryLoginScreen(modifier: Modifier) {
             loginViewModel.getInitialBalanceReward(),
         )
 
-    Box(modifier = modifier.padding(horizontal = 16.dp)) {
-        Image(
-            painter = painterResource(Res.drawable.login_top_shadow),
-            modifier = Modifier.align(Alignment.TopCenter),
-            contentDescription = null,
-            contentScale = ContentScale.None,
-        )
+    Box(modifier = modifier) {
+        Background()
         Box(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
             contentAlignment = Center,
         ) {
             SignupView(
@@ -78,6 +80,46 @@ fun MandatoryLoginScreen(modifier: Modifier) {
                 link = termsLinkState,
                 bottomSheetState = termsSheetState,
                 onDismissRequest = { termsLinkState = "" },
+            )
+        }
+    }
+}
+
+@Composable
+private fun Background() {
+    val isUnFolded = rememberScreenFoldStateProvider().isScreenUnfoldedFlow.collectAsState(false)
+    val bottomImageHeight = if (isUnFolded.value) 300.dp else 250.dp
+    val bottomImageAlignment = if (isUnFolded.value) BottomCenter else Center
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(Res.drawable.login_top_shadow),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier =
+                Modifier
+                    .align(Alignment.TopCenter)
+                    .height(150.dp)
+                    .fillMaxWidth(),
+        )
+        Box(modifier = Modifier.align(Alignment.BottomCenter)) {
+            Image(
+                painter = painterResource(Res.drawable.login_bottom_shadow),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier =
+                    Modifier
+                        .height(bottomImageHeight)
+                        .fillMaxWidth(),
+            )
+            Image(
+                painter = painterResource(Res.drawable.login_coins),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                alignment = bottomImageAlignment,
+                modifier =
+                    Modifier
+                        .height(bottomImageHeight)
+                        .fillMaxWidth(),
             )
         }
     }
