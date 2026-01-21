326a5a12f83863c155c422649838e76323823db9
samarth-paboowal-yral <samarth@gobazzinga.io>
2026-01-21 13:31:52 +0530

    build: adds subscriptions module

--- shared/app/build.gradle.kts
+++ shared/app/build.gradle.kts
@@ -59,6 +59,7 @@ kotlin {
             implementation(projects.shared.features.leaderboard)
             implementation(projects.shared.features.tournament)
             implementation(projects.shared.features.chat)
+            implementation(projects.shared.features.subscriptions)
 
             implementation(projects.shared.rust.service)
 
---
+++ shared/features/subscriptions/src/commonMain/kotlin/com/yral/shared/features/subscriptions/di/SubscriptionsModule.kt
@@ -0,0 +1,5 @@
+package com.yral.shared.features.subscriptions.di
+
+import org.koin.dsl.module
+
+val subscriptionsModule = module {}
