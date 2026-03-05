e96a51816c4a72957a7dc96a4b661e1bd5a51de3
shivam-bhavsar-yral <shivambhavsar@gobazzinga.io>
2026-03-05 15:46:26 +0530

    chore: Increments android app version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     namespace = "com.yral.android"
     defaultConfig {
         applicationId = "com.yral.android"
-        versionCode = 72
-        versionName = "2.7.0"
+        versionCode = 73
+        versionName = "2.7.1"
         ndkVersion = "28.0.13004108"
         buildConfigField(
             type = "String",
